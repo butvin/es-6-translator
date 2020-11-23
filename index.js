80a46a9cc7851a3047bde0abb33f4448ff88f304
butvin-home-sumsung <butvin-dev@gmail.com>
2020-11-23 05:06:53 +0200

    package structure & code-drafts

--- index.js
+++ index.js
@@ -1,34 +1,6 @@
 "use strict"
 import Translate from './translator.js';
 
-function format() {
-    const date = new Date();
-    const locale = 'zh-Hans-CN-bauddha-u-nu-hanidec';
-// const locale =  navigator.languages ? navigator.languages[0] : navigator.language;
-    const formatter = new Intl.DateTimeFormat(locale);
-    const formattedDate = formatter.format(date);
-    console.log(formattedDate); // 二〇二〇/四/二一
-
-    return formattedDate
-}
-
-function sprintf(s) {
-    let bits = s.split('%');
-    let out = bits[0];
-    const re = /^([ds])(.*)$/;
-    for (let i=1; i<bits.length; i++) {
-        // p = re.exec(bits[i]);
-        // if (!p || arguments[i]==null) continue;
-        // if (p[1] == 'd') {
-        //   out += parseInt(arguments[i], 10);
-        // } else if (p[1] == 's') {
-        //   out += arguments[i];
-        // }
-        // out += p[2];
-    }
-    return out;
-}
-
 const options = {
     defaultLang: 'ru',
     languages: ['ru', 'ukr'],
@@ -37,10 +9,123 @@ const options = {
     filesLocation: '/i18n/',
 }
 
+import * as RUS from './i18n/ru.js'
+import * as UKR from './i18n/ukr.js'
+
+const rusData = RUS.default
+console.log(rusData)
+
+const ukrData = UKR.default
+console.log(ukrData)
+
 
 const translator = new Translate(options)
 console.log(translator)
 
-
 translator.setLang('ukr')
 console.log(translator.getLang())
+
+
+
+// function i18n(template) {
+//     for (
+//         let info = i18n.db[i18n.locale][template.join('\x01')],
+//             out = [info.t[0]], i = 1,
+//             length = info.t.length;
+//         i < length;
+//         i++
+//     ) out[i] = arguments[1 + info.v[i - 1]] + info.t[i];
+//
+//     return out.join('');
+// }
+
+
+// i18n.locale = 'ru';
+// i18n.db = {};
+
+
+// i18n.set = locale => (tCurrent, ...rCurrent) => {
+//     const key = tCurrent.join('\x01');
+//     let db = i18n.db[locale] || (i18n.db[locale] = {});
+//     db[key] = {
+//         t: tCurrent.slice(),
+//         v: rCurrent.map((value, i) => i)
+//     };
+//     const config = {
+//         for: other => (tOther, ...rOther) => {
+//             db = i18n.db[other] || (i18n.db[other] = {});
+//             db[key] = {
+//                 t: tOther.slice(),
+//                 v: rOther.map((value, i) => rCurrent.indexOf(value))
+//             };
+//             return config;
+//         }
+//     };
+//     return config;
+// };
+
+
+// const rusData = fetch('/i18n/rus.json')
+// .then(response => response.json())
+// .then(data => {
+//   console.log(data)
+//   return data
+// })
+// console.log(rusData)
+
+
+//   {
+//     if (!response.ok || response.status !== 200) {
+//       throw new Error(`HTTP ERROR! STATUS: ${response.status}`)
+//   } else {
+//     console.log(response.json())
+//     localStorage.setItem('loaded', JSON.stringify(response))
+//     console.log(JSON.parse(localStorage.getItem('loaded')))
+//   }
+// }
+
+
+// if (lang === this._lang) {
+//   return console.log(`${this._lang} is already set.`)
+// }
+// fetch(`./i18n/${lang}.json`)
+// .then(response => response.json())
+// .then((translation) => {
+//   console.log(translation)
+// })
+// .catch((e) => {
+//   console.log(`Could not load ${this._lang}.json: ` + e.message)
+// })
+// .finally(() => {
+//   // console.log(`[FINALLY] Fetch from '${uri}' for ${this.getLang()} finished.`);
+// })
+
+
+// function format() {
+//     const date = new Date();
+//     const locale = 'zh-Hans-CN-bauddha-u-nu-hanidec';
+// // const locale =  navigator.languages ? navigator.languages[0] : navigator.language;
+//     const formatter = new Intl.DateTimeFormat(locale);
+//     const formattedDate = formatter.format(date);
+//     console.log(formattedDate); // 二〇二〇/四/二一
+//
+//     return formattedDate
+// }
+
+
+// function sprintf(s) {
+//     let bits = s.split('%');
+//     let out = bits[0];
+//     const re = /^([ds])(.*)$/;
+//     for (let i=1; i<bits.length; i++) {
+//         // p = re.exec(bits[i]);
+//         // if (!p || arguments[i]==null) continue;
+//         // if (p[1] == 'd') {
+//         //   out += parseInt(arguments[i], 10);
+//         // } else if (p[1] == 's') {
+//         //   out += arguments[i];
+//         // }
+//         // out += p[2];
+//     }
+//     return out;
+// }
--- translator.js
+++ translator.js
@@ -1,5 +1,201 @@
 "use strict";
 
 export default class Translate {
+    /**
+     * Translators constructor
+     *
+     * @param options
+     */
+    constructor (options = {}) {
+        this._cache = new Map();
 
+        if (options instanceof Object) {
+            this._options = Object.assign({}, this.defaultConfig, options)
+
+            if (this._options.autoDetectLang) {
+                this._lang = this._options.detectLang()
+                this._options.defaultLanguage = this.detectLang()
+            }
+
+            if (this._options.defaultLang &&
+                typeof this._options.defaultLang == 'string'
+            ) {
+                this.setLang(this._options.defaultLang)
+            }
+        }
+    }
+
+    /**
+     * Get loaded locale at start app
+     *
+     * @returns {string}
+     */
+    detectLang() {
+        const storedLanguage = localStorage.getItem('language')
+
+        if (this._options.localStorage && storedLanguage) {
+            return storedLanguage
+        }
+
+        const language = navigator.languages ?
+            navigator.languages[0] : navigator.language;
+
+        return language.slice(0, 2)
+    }
+
+    /**
+     *
+     * @param {Object} data
+     * @param {String} lang
+     * @returns {Translate}
+     */
+    addTranslation(data, lang) {
+        // todo
+    }
+
+    /**
+     * Examples
+     *
+     * translate(['%s день', '%s дня', '%s дней', 10]) // множественная форма
+     * translate('Привет')
+     * translate('Привет %s', 'гость')
+     *
+     * @param {String|Number|Array} msgId
+     * @returns {String|Translate}
+     */
+    translate(msgId) {
+        // todo
+    }
+
+    /**
+     * Current locale
+     *
+     * @param {string} lang
+     * @returns {Translate}
+     */
+    setLang(lang) {
+        if (!this._options.languages.includes(lang)) {
+            return
+        }
+
+        if (this._options.localStorage) {
+            localStorage.setItem('language', lang);
+        }
+
+        return this._lang = this.$loadDictionary(lang)
+    }
+
+    /**
+     * Get current loaded language
+     *
+     * @returns {string}
+     */
+    getLang() {
+        return this._lang
+    }
+
+    _fetch(path) {
+        return fetch(path)
+            .then(response => response.json())
+            .catch(error => {
+                console.error(`
+          Could not load "${path}".
+          Please make sure that the file exists.
+        `)
+            })
+    }
+
+    async $loadDictionary(language) {
+
+        if (this._cache.has(language)) {
+            return JSON.parse(this._cache.get(language))
+        }
+
+        const translation = await this._fetch(
+            `${this._options.filesLocation}/${language}.json`
+        )
+
+        if (!this._cache.has(language)) {
+            this._cache.set(language, JSON.stringify(translation));
+        }
+
+        return translation
+    }
+
+    // async load(language) {
+    //   if (!this._options.languages.includes(language)) {
+    //     return;
+    //   }
+    //
+    //   this._translate(await this.$loadDictionary(language));
+    //
+    //   document.documentElement.lang = language;
+    //
+    //   if (this._options.localStorage) {
+    //     localStorage.setItem('language', language);
+    //   }
+    // }
+
+    _getValueFromJSON(key, json, fallback) {
+        let text = key.split('.').reduce((obj, i) => obj[i], json);
+
+        if (!text && this._options.defaultLang && fallback) {
+            let fallbackTranslation = JSON.parse(
+                this._cache.get(this._options.defaultLang)
+            );
+            text = this._getValueFromJSON(key, fallbackTranslation, false);
+        } else if (!text) {
+            text = key;
+            console.warn(`Could not find text for attribute "${key}".`);
+        }
+
+        return text;
+    }
+
+    /**
+     * The defaultConfig property getter
+     */
+    get defaultConfig() {
+        return {
+            defaultLang: '',
+            languages: ['rus', 'ukr'],
+            filesLocation: '/i18n/',
+            localStorage: false,
+            autoDetectLang: false,
+        }
+    }
+
+    _translate(translation) {
+        const zip = (keys, values) => keys.map((key, i) => [key, values[i]])
+
+        const nullSafeSplit = (str, separator) => (str ? str.split(separator) : null)
+
+        let replace = element => {
+            let keys = nullSafeSplit(element.getAttribute("data-i18n"), " ") || [];
+            let properties = nullSafeSplit(
+                element.getAttribute("data-i18n-attr"),
+                " "
+            ) || ["innerHTML"];
+
+            if (keys.length > 0 && keys.length !== properties.length) {
+                console.error('data-i18n and data-i18n-attr must contain the same number of items');
+            } else {
+                const pairs = zip(keys, properties)
+
+                pairs.forEach(pair => {
+                    const [key, property] = pair;
+                    const text = this._getValueFromJSON(key, translation, true);
+
+                    if (text) {
+                        element[property] = text;
+                        element.setAttribute(property, text);
+                    } else {
+                        console.error(`Could not find text for attribute "${key}".`);
+                    }
+                });
+            }
+        }
+
+        this._elements.forEach(replace);
+    }
 }
